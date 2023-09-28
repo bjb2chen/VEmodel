8e046d7fc5bbaffe105f3f2fd5f21b2fdb66a94c
bjb2chen <bjb2chen@uwaterloo.ca>
2023-09-27 22:23:55 -0400

    Line 14

--- Toby_automate/sept26.py
+++ Toby_automate/sept26.py
@@ -17,5 +17,11 @@ print('The following arguments were passed to this ' + str(sys.argv[0])   \
 
 hessout = sys.argv[1]
 filnam = "nh3cat_ccd_gmcpt_7o7e_C3vinC1_3st_diab"
+target_str = ' TOTAL NUMBER OF ATOMS'
+
+with open(str(hessout), "r") as fp:
+	data = fp.read()
+
+target_index = data.find(target_str)
 
 ## natoms=`grep ' TOTAL NUMBER OF ATOMS' $hessout|cut -d'=' -f2`
