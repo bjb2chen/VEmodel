672948122fc401945d944e8370ec6cf2b8d69ab5
bjb2chen <bjb2chen@uwaterloo.ca>
2023-11-20 23:27:43 -0500

    Lesson 21

---
+++ python_training/lesson21.py
@@ -0,0 +1,8 @@
+### Lesson 21
+## string formatting
+
+name = 'Alice'
+place = 'Main Street'
+time = '6 pm'
+food = 'turnips'
+print('Hello %s, you are invited to a party at %s at %s. Please bring %s.' % (name, place, time, food))
