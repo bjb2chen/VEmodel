a449835fee80105dd9434ca4773be1de95c02b5c
bjb2chen <bjb2chen@uwaterloo.ca>
2023-12-31 16:56:58 -0500

    Complete lesson 28

--- python_training/lesson28.py
+++ python_training/lesson28.py
@@ -5,4 +5,15 @@ namesRegex = re.compile(r'Agent \w+')
 
 namesRegex = re.compile(r'Agent (\w)\w*')
 namesRegex.findall('Agent Alice gave the secret documents to Agent Bob.')
-namesRegex.sub(r'REDACTED \1****', 'Agent Alice gave the secret documents to Agent Bob.')
+namesRegex.sub(r'REDACTED \1*\*\*\*', 'Agent Alice gave the secret documents to Agent Bob.')
+
+
+re.compile(r'''
+	(\d\d\d-)|   # area code
+	(\(\d\d\d\) )# -or- area code with parens and no dash
+	-        # first dash
+	\d\d\d   # first 3 digits
+	-        # second dash
+	\d\d\d\d
+	\sx\d{2,4} # etension, like x1234
+	''', re.VERBOSE | re.DOTALL | re.IGNORECASE)
