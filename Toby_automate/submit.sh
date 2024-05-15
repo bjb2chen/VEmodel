3fc373fbfbfc8e6aa37b7b13a2f2ee3d750d3f42
bjb2chen <bjb2chen@uwaterloo.ca>
2024-05-15 13:13:20 -0400

    Add an echo statement for running MCTDH-86

--- Toby_automate/submit.sh
+++ Toby_automate/submit.sh
@@ -3,6 +3,8 @@
 # load mctdh
 # module load mctdh/84.16
 
+echo "Running MCTDH-86!"
+
 # call mctdh
 # mctdh84 -mnd -w ${1}
 /home/bjb2chen/LOCAL/mctdh/mctdh86.4/bin/binary/x86_64/mctdh86 -mnd -w ${1}
