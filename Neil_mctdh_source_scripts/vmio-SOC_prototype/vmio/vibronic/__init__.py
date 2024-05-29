e0c50f1d8348941791662085a800674a3bceecdc
bjb2chen <bjb2chen@uwaterloo.ca>
2024-05-28 21:35:05 -0400

    Add vIO_wrapper in __init__.py of vmio/vibronic/ **IMPORTANT FIX IF YOU WANT test_clean.py TO WORK**

--- Neil_mctdh_source_scripts/vmio-SOC_prototype/vmio/vibronic/__init__.py
+++ Neil_mctdh_source_scripts/vmio-SOC_prototype/vmio/vibronic/__init__.py
@@ -7,9 +7,11 @@ Scripts that handle data
 # from . import helper
 from . import vibronic_model_io as vIO
 from .vibronic_model_keys import VibronicModelKeys as VMK
+from . import wrapper_function as vIO_wrapper
 
 __all__ = [
     'vIO',
     'VMK',
+    'vIO_wrapper'
     # 'helper',
 ]
